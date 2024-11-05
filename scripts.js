83e728265492596883f3be84765355f1e7930eb3
Daniel-Cristian-de-jesus <danielcristiandejesus.gmail.com>
2024-11-05 10:37:44 -0300

    Meu primeiro projeto.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let formulario = document.querySelector(".formulario")
+function aparecerformulario(){
+formulario.style.left = "50%"
+}
+function desaparecerformulario(){
+formulario.style.left = "-50%"
+}
